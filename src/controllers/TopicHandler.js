433dd00ff1a08560a0a989f2545599ff664a11b6
Max Stubbe <maxyman@hotmail.nl>
2019-05-16 14:12:32 +0200

    inventory routes added

--- src/controllers/TopicHandler.js
+++ src/controllers/TopicHandler.js
@@ -38,7 +38,7 @@ exports.listen = function(exchange,topics) {amqp.connect('amqp://localhost', fun
 }
 
 function handleMessage(msg){
-    if(msg.fields.routingKey == "hello"){
+    if(msg.fields.routingKey.contains("inventory")){
         handleHelloMessage(msg);
     }
     if(msg.fields.routingKey == "doei"){
