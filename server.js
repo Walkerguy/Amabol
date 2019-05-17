1ca71fe83e6906b70f59b6ecd8af731bc863f8b8
Bram <shep.ukn@gmail.com>
2019-05-17 12:58:13 +0200

    Updated server.js, added UUID.

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-//basic sending en recieving 
-var sendController = require("./src/controllers/send");
-var receiveController = require("./src/controllers/receive");
 //publishing and subscribing
 var MessageHandler = require("./src/controllers/MessageHandler");
 var MessagePublisher = require("./src/controllers/MessagePublisher");
@@ -14,7 +11,7 @@ var TopicPublisher = require("./src/controllers/TopicPublisher");
 var Topics = ['#.inventory.#',"cool"] //topics to listen too
 
 
-var inventoryroutes= require('./src/routes/Inventory_routes');
+var OrderRoutes = require('./src/routes/OrderRoutes');
 
 app.use(bodyParser.urlencoded({
   'extended': 'true'
@@ -29,23 +26,24 @@ app.get('/', function (req, res) {
   res.send('<h1>Hello World from Nodejs!</h1>');
 });
 
-app.get('/send', sendController.sendMessage);
-app.get('/send/:text', sendController.sendMessage);
-
 app.get('/publish', MessagePublisher.sendMessage);
 app.get('/publish/:text', MessagePublisher.sendMessage);
 
 //app.get('/topic/:text/:topic', TopicPublisher.sendMessageWithTopic);
 
-app.get('/', function (req, res) {
+app.use('', OrderRoutes);
 
 var server = app.listen(8888, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  // Add listeners here.
+
   MessageHandler.listen("logs");
   TopicHandler.listen("topic_logs",Topics);
 
   console.log('Example app listening at http://%s:%s', host, port);
+  
+  const uuidv1 = require('uuid/v1'); 
+  console.log(uuidv1());
+
 });
