095a3543a746ce194de2fb10992fbf39360efb32
Erin Wallaard <erin.wallaard@opensesameict.nl>
2019-05-21 17:53:20 +0200

    haha

--- model/event.model.js
+++ model/event.model.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const EventSchema = new Schema({
   event: {type:String, required: true},
   date: {type:Date, required: true},
+  topic: {type: String, required: true},
 })
 
 const Event = mongoose.model('event', EventSchema);
