8a4eb5b343281de087d2161d96d763f07783beeb
Max Stubbe <maxyman@hotmail.nl>
2019-05-22 15:35:49 +0200

    poort veranderd

--- src/config/env/env.js
+++ src/config/env/env.js
@@ -1,10 +1,10 @@
 var env = {
     webPort: '3000',
     dbHost: '128.199.61.247',
-    dbPort: '27020',
+    dbPort: '27021',
     dbUser: '',
     dbPassword: '',
-    dbDatabase: 'shoppingcart'
+    dbDatabase: 'inventory'
 }
 
 var dburl = 'mongodb://' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase;
